062c9c9d63369b13feb371be948358287ba3083c
Lewis Jackson-Mcketty <ljackson-mcketty1@sheffield.ac.uk>
2016-12-11 02:18:15 +0000

    Managed to create board of randomly coloured squares

--- floodit.rb
+++ floodit.rb
@@ -5,46 +5,85 @@
 # To change this template use Tools | Templates.
 
 def get_board(width, height)
-  # TODO: Implement this method
-  #
-  # This method should return a two-dimensional array.
-  # Each element of the array should be one of the
-  # following values (These are "symbols", you can use
-  # them like constant values):
-  # :red
-  # :blue
-  # :green
-  # :yellow
-  # :cyan
-  # :magenta
-  #
-  # It is important that this method is used because
-  # this will be used for checking the functionality
-  # of your implementation.
+  spaces = Array.new(width, "  ")
+  table = Array.new(height, spaces)
+  i=0
+  while i<table.length do
+    j=0
+    while j<spaces.length do
+      color = rand(1..6)
+      if color == 1 then
+        print table[i][j].colorize( :background => :red)
+      elsif color == 2 then
+        print table[i][j].colorize( :background => :blue)
+      elsif color == 3 then
+        print table[i][j].colorize( :background => :green)
+      elsif color == 4 then
+        print table[i][j].colorize( :background => :cyan)
+      elsif color == 5 then
+        print table[i][j].colorize( :background => :yellow)
+      else
+        print table[i][j].colorize( :background => :magenta)
+      end
+      j+=1
+    end
+    puts ""
+    i+=1
+  end     
 end
 
 # TODO: Implement everything else as described in the
 #       assignment brief.
+  
 
 
-class FloodIt
+def main_menu(width, height)
+  columns = width
+  rows = height
+  puts "  Main menu: "
+    puts "  Enter 's' to start game"
+    puts "  Enter 'c' to change size of board"
+    puts "  Enter 'q' to quit"
+    puts "  Size of board= #{width} x #{height}"
+    loop do
+      puts ""
+      print "->"  
+      input = gets.chomp
+      if input == "q" then
+        exit
+      elsif input == "s" then
+        get_board(columns, rows)
+      elsif input == "c"
+        print "  How wide do you want the game: "
+        columns = gets.chomp.to_i
+        print "  How high do you want the game: "
+        rows = gets.chomp.to_i
+        puts ""
+        main_menu(columns, rows)
+    end
+  end       
+end
+    
+    
   require 'console_splash'
   splash = ConsoleSplash.new()
-  splash = ConsoleSplash.new(30, 78)a
+  splash = ConsoleSplash.new(30, 78)
   splash.write_header("Flood It", "Lewis Jackson-Mcketty", "0.1",
         {:nameFg=>:red, :authorFg=>:green})
   splash.write_center(-5, "Press Enter to start")
   splash.write_horizontal_pattern("*", {:fg=>:white, :bg=>:green})
   splash.write_vertical_pattern("|")
- 
+  
+  number_games = 0
+  best_score = 0
+  columns = 14
+  rows = 9
+  
   splash.splash
   puts ""
   puts ""
   print "->"  
   input = gets.chomp
-  
-  number_games = 0
-  best_score = 0
   
   while input != "" do
     puts ""
@@ -52,41 +91,12 @@ class FloodIt
     input = gets.chomp
   end
   
-  if input == "" then
-    puts "  Main menu: "
-    puts "  Enter 's' to start game"
-    puts "  Enter 'c' to change size of board"
-    puts "  Enter 'q' to quit"
-    if number_games == 0 then
-      puts "  No games played yet"
-    else 
-      puts "  Best score: #{best_score} turns"
-    end
-  end
+  main_menu(columns, rows)
+
   
-  columns = 14
-  rows = 9
+  
+  puts "Choose your initial colour"
+  print "->"
+  initial = gets.chomp
     
-  loop do
-    puts ""
-    print "->"  
-    input = gets.chomp
-    if input == "q" then
-      "puts quit game ygm"
-      break
-    elsif input == "s" then
-      puts "start of game"
-      break
-    elsif input == "c"
-    print "  How wide do you want the game: "
-    width = gets.chomp.to_i
-    print "  How high do you want the game: "
-    height = gets.chomp.to_i
-    columns = width
-    rows = height
-    puts "  #{columns} x #{rows}"
-    break
-    end
-  end
       
-end
